cd85d5b937c23b04771e6a226574946464f957af
Jennifer Luu <Jferluu@Jennifers-MacBook-Air-4.local>
2016-07-23 17:43:43 -0700

    changing the default location to Golden Gate Park

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+var map = L.map('mapid').setView([37.768611, -122.490113], 16);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
